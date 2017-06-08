13e59a08279217fbab1965c9ee096e09a6cf7b67
Pat Myron <pmyron09@gmail.com>
2017-06-08 15:55:18 -0400

    initial commit

---
+++ app/src/main/java/com/patmyron/blackbox/MainActivity.java
@@ -0,0 +1,186 @@
+package com.patmyron.blackbox;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.AudioManager;
+import android.media.MediaRecorder;
+import android.os.BatteryManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SoundMeter sm;
+    private float last_x, last_y, last_z;
+    long lastUpdate;
+    SensorManager mSensorManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        int[] sensors = new int[]{Sensor.TYPE_GRAVITY, Sensor.TYPE_PROXIMITY, Sensor.TYPE_LIGHT, Sensor.TYPE_GYROSCOPE, Sensor.TYPE_LINEAR_ACCELERATION};
+        for (int sensor : sensors) {
+            mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(sensor), SensorManager.SENSOR_DELAY_UI);
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+        } else if (sm == null) {
+            sm = new SoundMeter();
+            sm.start();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        switch (event.sensor.getType()) {
+            case Sensor.TYPE_GRAVITY:
+                ((TextView) findViewById(R.id.tv1)).setText("X: " + event.values[0]);
+                ((TextView) findViewById(R.id.tv2)).setText("Y: " + event.values[1]);
+                ((TextView) findViewById(R.id.tv3)).setText("Z: " + event.values[2]);
+                break;
+            case Sensor.TYPE_PROXIMITY:
+                ((TextView) findViewById(R.id.tv7)).setText("PROXIMITY: " + event.values[0]);
+                break;
+            case Sensor.TYPE_LIGHT:
+                ((TextView) findViewById(R.id.tv11)).setText("LIGHT: " + event.values[0]);
+                break;
+            case Sensor.TYPE_GYROSCOPE:
+                ((TextView) findViewById(R.id.tv14)).setText("X: " + event.values[0]);
+                ((TextView) findViewById(R.id.tv15)).setText("Y: " + event.values[1]);
+                ((TextView) findViewById(R.id.tv16)).setText("Z: " + event.values[2]);
+                break;
+            case Sensor.TYPE_LINEAR_ACCELERATION:
+                long curTime = System.currentTimeMillis();
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+                float speed = Math.abs(x + y + z - last_x - last_y - last_z) / (curTime - lastUpdate) * 10000;
+                if (speed > 8000) {
+                    ((TextView) findViewById(R.id.tv17)).setText("SHOOK");
+                }
+                last_x = x;
+                last_y = y;
+                last_z = z;
+                lastUpdate = curTime;
+                break;
+        }
+        try {
+            ((TextView) findViewById(R.id.tv4)).setText("BRIGHTNESS: " + android.provider.Settings.System.getInt(getContentResolver(), android.provider.Settings.System.SCREEN_BRIGHTNESS));
+        } catch (SettingNotFoundException e) {
+            e.printStackTrace();
+        }
+        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        ((TextView) findViewById(R.id.tv5)).setText("HEADSET: " + am.isWiredHeadsetOn());
+        ((TextView) findViewById(R.id.tv6)).setText("VOLUME: " + am.getStreamVolume(AudioManager.STREAM_MUSIC));
+        ((TextView) findViewById(R.id.tv8)).setText("AIRPLANE: " + isAirplaneModeOn(this));
+        batteryLevel();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED && sm != null) {
+            ((TextView) findViewById(R.id.tv10)).setText("NOISE: " + sm.getAmplitude());
+        }
+        ((TextView) findViewById(R.id.tv12)).setText("CLIPBOARD: " + ((ClipboardManager) getSystemService(CLIPBOARD_SERVICE)).getText());
+    }
+
+    public static boolean isAirplaneModeOn(Context context) {
+        return Settings.Global.getInt(context.getContentResolver(),
+                Settings.Global.AIRPLANE_MODE_ON, 0) != 0;
+    }
+
+    private void batteryLevel() {
+        BroadcastReceiver batteryLevelReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                context.unregisterReceiver(this);
+                int rawLevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+                int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+                int level = -1;
+                if (rawLevel >= 0 && scale > 0) {
+                    level = (rawLevel * 100) / scale;
+                }
+                ((TextView) findViewById(R.id.tv9)).setText("BATTERY: " + level);
+            }
+        };
+        IntentFilter batteryLevelFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(batteryLevelReceiver, batteryLevelFilter);
+    }
+
+    public class SoundMeter {
+        private MediaRecorder mRecorder = null;
+
+        public void start() {
+            if (mRecorder == null) {
+                mRecorder = new MediaRecorder();
+                mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                mRecorder.setOutputFile("/dev/null");
+                try {
+                    mRecorder.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                mRecorder.start();
+            }
+        }
+
+        public void stop() {
+            if (mRecorder != null) {
+                mRecorder.stop();
+                mRecorder.release();
+                mRecorder = null;
+            }
+        }
+
+        public double getAmplitude() {
+            if (mRecorder != null)
+                return mRecorder.getMaxAmplitude();
+            else
+                return 0;
+        }
+    }
+
+    public static class MyReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.e("pmyron", "SCREENSHOT");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        sm = new SoundMeter();
+        sm.start();
+    }
+
+}
