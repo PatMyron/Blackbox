4bcd5bb26982d9533b64ff8a9ea8ea3a88a43f20
Pat Myron <pmyron09@gmail.com>
2017-06-27 01:04:25 -0400

    adding location

--- app/src/main/java/com/patmyron/blackbox/MainActivity.java
+++ app/src/main/java/com/patmyron/blackbox/MainActivity.java
@@ -11,6 +11,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
 import android.media.AudioManager;
 import android.media.MediaRecorder;
 import android.os.BatteryManager;
@@ -22,15 +23,20 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 import java.io.IOException;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    private SoundMeter sm;
-    private float last_x, last_y, last_z;
     long lastUpdate;
     SensorManager mSensorManager;
+    private SoundMeter sm;
+    private float last_x, last_y, last_z;
+    private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +51,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         for (int sensor : new int[]{Sensor.TYPE_GRAVITY, Sensor.TYPE_PROXIMITY, Sensor.TYPE_LIGHT, Sensor.TYPE_GYROSCOPE, Sensor.TYPE_LINEAR_ACCELERATION}) {
             mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(sensor), SensorManager.SENSOR_DELAY_UI);
         }
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
-        } else if (sm == null) {
-            sm = new SoundMeter();
-            sm.start();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.RECORD_AUDIO}, 1);
+        } else {
+            if (sm == null) {
+                sm = new SoundMeter();
+                sm.start();
+            }
+            if (mFusedLocationClient == null) {
+                mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+            }
         }
         batteryBroadcastReceiver();
         screenshotBroadcastReceiver();
@@ -68,20 +79,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onSensorChanged(SensorEvent event) {
         switch (event.sensor.getType()) {
             case Sensor.TYPE_GRAVITY:
-                ((TextView) findViewById(R.id.tv1)).setText("X: " + event.values[0]);
+                ((TextView) findViewById(R.id.tv1)).setText("GRAVITY        X: " + event.values[0]);
                 ((TextView) findViewById(R.id.tv2)).setText("Y: " + event.values[1]);
                 ((TextView) findViewById(R.id.tv3)).setText("Z: " + event.values[2]);
+                break;
+            case Sensor.TYPE_GYROSCOPE:
+                ((TextView) findViewById(R.id.tv4)).setText("GYROSCOPE        X: " + event.values[0]);
+                ((TextView) findViewById(R.id.tv5)).setText("Y: " + event.values[1]);
+                ((TextView) findViewById(R.id.tv6)).setText("Z: " + event.values[2]);
                 break;
             case Sensor.TYPE_PROXIMITY:
                 ((TextView) findViewById(R.id.tv7)).setText("PROXIMITY: " + event.values[0]);
                 break;
             case Sensor.TYPE_LIGHT:
                 ((TextView) findViewById(R.id.tv11)).setText("LIGHT: " + event.values[0]);
-                break;
-            case Sensor.TYPE_GYROSCOPE:
-                ((TextView) findViewById(R.id.tv14)).setText("X: " + event.values[0]);
-                ((TextView) findViewById(R.id.tv15)).setText("Y: " + event.values[1]);
-                ((TextView) findViewById(R.id.tv16)).setText("Z: " + event.values[2]);
                 break;
             case Sensor.TYPE_LINEAR_ACCELERATION:
                 long curTime = System.currentTimeMillis();
@@ -99,19 +110,32 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 break;
         }
         try {
-            ((TextView) findViewById(R.id.tv4)).setText("BRIGHTNESS: " + android.provider.Settings.System.getInt(getContentResolver(), android.provider.Settings.System.SCREEN_BRIGHTNESS));
+            ((TextView) findViewById(R.id.tv14)).setText("BRIGHTNESS: " + android.provider.Settings.System.getInt(getContentResolver(), android.provider.Settings.System.SCREEN_BRIGHTNESS));
         } catch (SettingNotFoundException e) {
             e.printStackTrace();
         }
         AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        ((TextView) findViewById(R.id.tv5)).setText("HEADSET: " + am.isWiredHeadsetOn());
-        ((TextView) findViewById(R.id.tv6)).setText("VOLUME: " + am.getStreamVolume(AudioManager.STREAM_MUSIC));
+        ((TextView) findViewById(R.id.tv15)).setText("HEADSET: " + am.isWiredHeadsetOn());
+        ((TextView) findViewById(R.id.tv16)).setText("VOLUME: " + am.getStreamVolume(AudioManager.STREAM_MUSIC));
         ((TextView) findViewById(R.id.tv8)).setText("AIRPLANE: " + Settings.Global.getInt(getContentResolver(), Settings.Global.AIRPLANE_MODE_ON, 0));
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED && sm != null) {
             ((TextView) findViewById(R.id.tv10)).setText("NOISE: " + sm.getAmplitude());
         }
         ((TextView) findViewById(R.id.tv12)).setText("CLIPBOARD: " + ((ClipboardManager) getSystemService(CLIPBOARD_SERVICE)).getText());
         ((TextView) findViewById(R.id.tv18)).setText("DATETIME: " + new Date());
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && mFusedLocationClient != null) {
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                ((TextView) findViewById(R.id.tv19)).setText("Latitude: " + location.getLatitude());
+                                ((TextView) findViewById(R.id.tv20)).setText("Longitude: " + location.getLongitude());
+                            }
+                        }
+                    });
+        }
     }
 
     private void batteryBroadcastReceiver() {
@@ -141,6 +165,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         IntentFilter filter = new IntentFilter(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
         filter.addDataScheme("file");
         registerReceiver(receiver, filter);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        sm = new SoundMeter();
+        sm.start();
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
     private class SoundMeter {
@@ -176,12 +207,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             else
                 return 0;
         }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        sm = new SoundMeter();
-        sm.start();
     }
 
 }
