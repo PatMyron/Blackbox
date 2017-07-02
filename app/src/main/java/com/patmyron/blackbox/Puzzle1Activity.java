cefe592754576a938825fdb71d54e7b8a9d75f07
Pat Myron <pmyron09@gmail.com>
2017-07-01 23:47:58 -0400

    unnecessary code in puzzle 1

--- app/src/main/java/com/patmyron/blackbox/Puzzle1Activity.java
+++ app/src/main/java/com/patmyron/blackbox/Puzzle1Activity.java
@@ -1,15 +1,12 @@
 package com.patmyron.blackbox;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.graphics.drawable.AnimationDrawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -29,12 +26,7 @@ public class Puzzle1Activity extends AppCompatActivity implements SensorEventLis
     protected void onResume() {
         super.onResume();
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        for (int sensor : new int[]{Sensor.TYPE_GRAVITY, Sensor.TYPE_PROXIMITY, Sensor.TYPE_LIGHT, Sensor.TYPE_GYROSCOPE, Sensor.TYPE_LINEAR_ACCELERATION}) {
-            mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(sensor), SensorManager.SENSOR_DELAY_UI);
-        }
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.RECORD_AUDIO}, 1);
-        }
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY), SensorManager.SENSOR_DELAY_UI);
     }
 
     @Override
@@ -48,24 +40,20 @@ public class Puzzle1Activity extends AppCompatActivity implements SensorEventLis
     }
 
     public void onSensorChanged(SensorEvent event) {
-        switch (event.sensor.getType()) {
-            case Sensor.TYPE_GRAVITY:
-                if (event.values[0] < -9.7) {
-                    animation(0);
-                } else if (event.values[0] > 9.7) {
-                    animation(1);
-                }
-                if (event.values[1] < -9.7) {
-                    animation(2);
-                } else if (event.values[1] > 9.7) {
-                    animation(3);
-                }
-                if (event.values[2] < -9.7) {
-                    animation(4);
-                } else if (event.values[2] > 9.7) {
-                    animation(5);
-                }
-                break;
+        if (event.values[0] < -9.7) {
+            animation(0);
+        } else if (event.values[0] > 9.7) {
+            animation(1);
+        }
+        if (event.values[1] < -9.7) {
+            animation(2);
+        } else if (event.values[1] > 9.7) {
+            animation(3);
+        }
+        if (event.values[2] < -9.7) {
+            animation(4);
+        } else if (event.values[2] > 9.7) {
+            animation(5);
         }
     }
 
