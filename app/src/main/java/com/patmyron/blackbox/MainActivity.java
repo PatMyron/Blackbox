e131edd2c084f805a8b3e3efebabf5e87a565459
Pat Myron <pmyron09@gmail.com>
2017-07-01 22:23:14 -0400

    puzzle map and moving 1st puzzle to another activity 🥂

--- app/src/main/java/com/patmyron/blackbox/MainActivity.java
+++ app/src/main/java/com/patmyron/blackbox/MainActivity.java
@@ -1,23 +1,12 @@
 package com.patmyron.blackbox;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.graphics.drawable.AnimationDrawable;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.view.View;
 
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
-
-    SensorManager mSensorManager;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,56 +15,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        for (int sensor : new int[]{Sensor.TYPE_GRAVITY, Sensor.TYPE_PROXIMITY, Sensor.TYPE_LIGHT, Sensor.TYPE_GYROSCOPE, Sensor.TYPE_LINEAR_ACCELERATION}) {
-            mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(sensor), SensorManager.SENSOR_DELAY_UI);
-        }
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.RECORD_AUDIO}, 1);
-        }
-    }
-
-    @Override
     protected void onPause() {
         super.onPause();
-        mSensorManager.unregisterListener(this);
-//        if (mSpeechRecognizer != null) {
-//            mSpeechRecognizer.destroy();
-//        }
     }
 
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+    public void puzzle1(View view) {
+        Intent intent = new Intent(this, Puzzle1Activity.class);
+        startActivity(intent);
     }
-
-    public void onSensorChanged(SensorEvent event) {
-        switch (event.sensor.getType()) {
-            case Sensor.TYPE_GRAVITY:
-                if (event.values[0] < -9.7) {
-                    animation(0);
-                } else if (event.values[0] > 9.7) {
-                    animation(1);
-                }
-                if (event.values[1] < -9.7) {
-                    animation(2);
-                } else if (event.values[1] > 9.7) {
-                    animation(3);
-                }
-                if (event.values[2] < -9.7) {
-                    animation(4);
-                } else if (event.values[2] > 9.7) {
-                    animation(5);
-                }
-                break;
-        }
-    }
-
-    private void animation(int index) {
-        ImageView iv = (ImageView) ((RelativeLayout) findViewById(R.id.ll)).getChildAt(index);
-        iv.setBackgroundResource(R.drawable.animation);
-        ((AnimationDrawable) iv.getBackground()).start();
-    }
-
 }
---
+++ app/src/main/java/com/patmyron/blackbox/Puzzle1Activity.java
@@ -0,0 +1,78 @@
+package com.patmyron.blackbox;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.AnimationDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+
+public class Puzzle1Activity extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager mSensorManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puzzle1);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        for (int sensor : new int[]{Sensor.TYPE_GRAVITY, Sensor.TYPE_PROXIMITY, Sensor.TYPE_LIGHT, Sensor.TYPE_GYROSCOPE, Sensor.TYPE_LINEAR_ACCELERATION}) {
+            mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(sensor), SensorManager.SENSOR_DELAY_UI);
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.RECORD_AUDIO}, 1);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        switch (event.sensor.getType()) {
+            case Sensor.TYPE_GRAVITY:
+                if (event.values[0] < -9.7) {
+                    animation(0);
+                } else if (event.values[0] > 9.7) {
+                    animation(1);
+                }
+                if (event.values[1] < -9.7) {
+                    animation(2);
+                } else if (event.values[1] > 9.7) {
+                    animation(3);
+                }
+                if (event.values[2] < -9.7) {
+                    animation(4);
+                } else if (event.values[2] > 9.7) {
+                    animation(5);
+                }
+                break;
+        }
+    }
+
+    private void animation(int index) {
+        ImageView iv = (ImageView) ((RelativeLayout) findViewById(R.id.ll)).getChildAt(index);
+        iv.setBackgroundResource(R.drawable.animation);
+        ((AnimationDrawable) iv.getBackground()).start();
+    }
+
+}
