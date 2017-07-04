4d519b5e1de7fcd87d7d3ebe1fee4f6848183164
Pat Myron <pmyron09@gmail.com>
2017-07-03 23:39:53 -0400

    puzzle6

---
+++ app/src/main/java/com/patmyron/blackbox/Puzzle6Activity.java
@@ -0,0 +1,44 @@
+package com.patmyron.blackbox;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+public class Puzzle6Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puzzle6);
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        screenshotBroadcastReceiver();
+    }
+
+    private void screenshotBroadcastReceiver() {
+        BroadcastReceiver receiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                context.unregisterReceiver(this);
+                animation(0);
+            }
+        };
+        IntentFilter filter = new IntentFilter(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        filter.addDataScheme("file");
+        registerReceiver(receiver, filter);
+    }
+
+    private void animation(int index) {
+        ImageView iv = (ImageView) ((RelativeLayout) findViewById(R.id.ll)).getChildAt(index);
+        iv.setBackgroundResource(R.drawable.animation);
+        ((AnimationDrawable) iv.getBackground()).start();
+    }
+}
