be74658ecb88717cbc93bc08a89fd7d16e521d56
Pat Myron <pmyron09@gmail.com>
2017-07-03 23:17:32 -0400

    filling squares for now (Hack)

--- app/src/main/java/com/patmyron/blackbox/MainActivity.java
+++ app/src/main/java/com/patmyron/blackbox/MainActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +26,27 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent();
         intent.setClassName(this, this.getPackageName() + view.getTag());
         this.startActivity(intent);
+        ArrayList<ImageView> ivs = getViewsByTag((ViewGroup) view.getParent(), (String) view.getTag());
+        for (ImageView iv: ivs) {
+            iv.setImageResource(R.drawable.filled);
+        }
+    }
+
+    private static ArrayList<ImageView> getViewsByTag(ViewGroup root, String tag) {
+        ArrayList<ImageView> views = new ArrayList<>();
+        final int childCount = root.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            final View child = root.getChildAt(i);
+            if (child instanceof ViewGroup) {
+                views.addAll(getViewsByTag((ViewGroup) child, tag));
+            }
+
+            final Object tagObj = child.getTag();
+            if (tagObj != null && tagObj.equals(tag)) {
+                views.add((ImageView) child);
+            }
+
+        }
+        return views;
     }
 }
