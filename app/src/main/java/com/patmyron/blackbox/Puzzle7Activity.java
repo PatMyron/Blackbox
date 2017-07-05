8fbe1b75c83876be45459b5351dbdb7136650ee4
Pat Myron <pmyron09@gmail.com>
2017-07-04 22:40:18 -0400

    cleanup + adding special animation() func for puzzle9

--- app/src/main/java/com/patmyron/blackbox/Puzzle7Activity.java
+++ app/src/main/java/com/patmyron/blackbox/Puzzle7Activity.java
@@ -1,7 +1,7 @@
 package com.patmyron.blackbox;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class Puzzle7Activity extends AppCompatActivity {
 
--- app/src/main/java/com/patmyron/blackbox/Puzzle9Activity.java
+++ app/src/main/java/com/patmyron/blackbox/Puzzle9Activity.java
@@ -1,7 +1,10 @@
 package com.patmyron.blackbox;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class Puzzle9Activity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class Puzzle9Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle9);
+    }
+
+    private void animation(int index) {
+        ImageView iv = (ImageView) ((LinearLayout) ((LinearLayout) findViewById(R.id.ll)).getChildAt(index)).getChildAt(0);
+        iv.setBackgroundResource(R.drawable.animation);
+        ((AnimationDrawable) iv.getBackground()).start();
     }
 }
