a9c2cc8a0f806efd91307b509ce1addfad50d149
Pat Myron <pmyron09@gmail.com>
2017-07-04 22:43:35 -0400

    animation() function for rest of puzzles

--- app/src/main/java/com/patmyron/blackbox/Puzzle10Activity.java
+++ app/src/main/java/com/patmyron/blackbox/Puzzle10Activity.java
@@ -1,7 +1,10 @@
 package com.patmyron.blackbox;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class Puzzle10Activity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class Puzzle10Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle10);
+    }
+
+    private void animation(int index) {
+        ImageView iv = (ImageView) ((RelativeLayout) findViewById(R.id.ll)).getChildAt(index);
+        iv.setBackgroundResource(R.drawable.animation);
+        ((AnimationDrawable) iv.getBackground()).start();
     }
 }
