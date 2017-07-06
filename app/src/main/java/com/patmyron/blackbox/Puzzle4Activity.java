5cbcd0370badd65d599a40fcca560bc204b93460
Pat Myron <pmyron09@gmail.com>
2017-07-05 23:38:25 -0400

    puzzle4 animation

--- app/src/main/java/com/patmyron/blackbox/Puzzle4Activity.java
+++ app/src/main/java/com/patmyron/blackbox/Puzzle4Activity.java
@@ -8,12 +8,17 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class Puzzle4Activity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager mSensorManager;
+    private Animation mAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,13 @@ public class Puzzle4Activity extends AppCompatActivity implements SensorEventLis
         super.onResume();
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY), SensorManager.SENSOR_DELAY_UI);
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
+        mAnimation = new AlphaAnimation(1, 0);
+        mAnimation.setDuration(2000);
+        mAnimation.setInterpolator(new AccelerateInterpolator());
+        mAnimation.setRepeatCount(Animation.INFINITE);
+        mAnimation.setRepeatMode(Animation.REVERSE);
+        iv.startAnimation(mAnimation);
     }
 
     @Override
@@ -39,8 +51,14 @@ public class Puzzle4Activity extends AppCompatActivity implements SensorEventLis
     }
 
     public void onSensorChanged(SensorEvent event) {
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
         if (event.values[0] < 4) {
+            iv.clearAnimation();
+            iv.setVisibility(View.INVISIBLE);
             animation(0);
+        } else {
+            iv.startAnimation(mAnimation);
+            iv.setVisibility(View.VISIBLE);
         }
     }
 
