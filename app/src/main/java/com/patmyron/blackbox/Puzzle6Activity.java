42ce146665e5f1484abddc73b8b67b6bb4c7b539
Pat Myron <pmyron09@gmail.com>
2017-11-04 16:10:25 -0700

    even in a broadcast receiver 😱

--- app/src/main/java/com/patmyron/blackbox/Puzzle6Activity.java
+++ app/src/main/java/com/patmyron/blackbox/Puzzle6Activity.java
@@ -1,14 +1,14 @@
 package com.patmyron.blackbox;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
+
+import static com.patmyron.blackbox.MainActivity.animation;
 
 public class Puzzle6Activity extends AppCompatActivity {
 
@@ -35,17 +35,11 @@ public class Puzzle6Activity extends AppCompatActivity {
     private void screenshotBroadcastReceiver() {
         receiver = new BroadcastReceiver() {
             public void onReceive(Context context, Intent intent) {
-                animation(0);
+                animation((Activity) context, 0);
             }
         };
         IntentFilter filter = new IntentFilter(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
         filter.addDataScheme("file");
         registerReceiver(receiver, filter);
-    }
-
-    private void animation(int index) {
-        ImageView iv = (ImageView) ((RelativeLayout) findViewById(R.id.ll)).getChildAt(index);
-        iv.setBackgroundResource(R.drawable.animation);
-        ((AnimationDrawable) iv.getBackground()).start();
     }
 }
