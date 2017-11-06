838ff27662bee1cf740399c1c91de04af97980bd
Pat Myron <pmyron09@gmail.com>
2017-11-05 20:31:24 -0800

    cleanup

--- app/src/main/java/com/patmyron/blackbox/MainActivity.java
+++ app/src/main/java/com/patmyron/blackbox/MainActivity.java
@@ -15,7 +15,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 
