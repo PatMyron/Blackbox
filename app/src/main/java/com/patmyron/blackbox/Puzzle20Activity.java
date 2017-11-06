5ab77e515a095f9d4b4b23a91647a2c9ebaeacca
Pat Myron <pmyron09@gmail.com>
2017-11-05 23:37:33 -0800

    power off puzzle 20
    might wanna test this somehow ;)

--- app/src/main/java/com/patmyron/blackbox/Puzzle20Activity.java
+++ app/src/main/java/com/patmyron/blackbox/Puzzle20Activity.java
@@ -1,13 +1,44 @@
 package com.patmyron.blackbox;
 
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import static com.patmyron.blackbox.MainActivity.animation;
+
 public class Puzzle20Activity extends AppCompatActivity {
+
+    private BroadcastReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle20);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        shutdownReceiver();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+    private void shutdownReceiver() {
+        receiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                animation((Activity) context, 0);
+            }
+        };
+        IntentFilter filter = new IntentFilter(Intent.ACTION_SHUTDOWN);
+        registerReceiver(receiver, filter);
     }
 }
