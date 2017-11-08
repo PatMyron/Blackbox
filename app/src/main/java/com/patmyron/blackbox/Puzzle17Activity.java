bd386a37094ac02b4aafff41cff09fe5aa839dab
Pat Myron <pmyron09@gmail.com>
2017-11-07 19:33:03 -0800

    shaking puzzle

--- app/src/main/java/com/patmyron/blackbox/Puzzle17Activity.java
+++ app/src/main/java/com/patmyron/blackbox/Puzzle17Activity.java
@@ -1,13 +1,25 @@
 package com.patmyron.blackbox;
 
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class Puzzle17Activity extends AppCompatActivity {
+import com.squareup.seismic.ShakeDetector;
+
+import static com.patmyron.blackbox.MainActivity.animation;
+
+public class Puzzle17Activity extends AppCompatActivity implements ShakeDetector.Listener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle17);
+
+        new ShakeDetector(this).start((SensorManager) getSystemService(SENSOR_SERVICE));
+    }
+
+    @Override
+    public void hearShake() {
+        animation(this, 0);
     }
 }
