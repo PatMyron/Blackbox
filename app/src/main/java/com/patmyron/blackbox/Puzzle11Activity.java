d8728882cdd7b5059a55846c099711b8189b5db2
Pat Myron <pmyron09@gmail.com>
2017-11-07 22:55:53 -0800

    mooncalc

--- app/src/main/java/com/patmyron/blackbox/Puzzle11Activity.java
+++ app/src/main/java/com/patmyron/blackbox/Puzzle11Activity.java
@@ -3,11 +3,16 @@ package com.patmyron.blackbox;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import org.shredzone.commons.suncalc.MoonIllumination;
+
 public class Puzzle11Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle11);
+        MoonIllumination.compute()
+                .now()
+                .execute();
     }
 }
