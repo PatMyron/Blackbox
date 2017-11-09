c5ddc4618baa639afd3a9232cce72a7b6a8ab5db
Pat Myron <pmyron09@gmail.com>
2017-11-08 22:58:08 -0800

    moon animation

--- app/src/main/java/com/patmyron/blackbox/Puzzle11Activity.java
+++ app/src/main/java/com/patmyron/blackbox/Puzzle11Activity.java
@@ -5,14 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 
 import org.shredzone.commons.suncalc.MoonIllumination;
 
+import static com.patmyron.blackbox.MainActivity.animation;
+
 public class Puzzle11Activity extends AppCompatActivity {
+
+    private static final double THRESHOLD = 4.0 / 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle11);
-        MoonIllumination.compute()
+        MoonIllumination moonIllumination = MoonIllumination.compute()
                 .now()
                 .execute();
+        if (moonIllumination.getFraction() < THRESHOLD) animation(this, 1);
+        if (moonIllumination.getFraction() > (1 - THRESHOLD)) animation(this, 2);
     }
 }
