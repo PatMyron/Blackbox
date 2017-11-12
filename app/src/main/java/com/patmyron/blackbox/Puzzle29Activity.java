6caa480ac38fd34f445c5a0cdab298726618b1f5
Pat Myron <pmyron09@gmail.com>
2017-11-12 14:24:56 -0800

    hold down puzzle

--- app/src/main/java/com/patmyron/blackbox/Puzzle29Activity.java
+++ app/src/main/java/com/patmyron/blackbox/Puzzle29Activity.java
@@ -4,7 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static com.patmyron.blackbox.MainActivity.animation;
+
 public class Puzzle29Activity extends AppCompatActivity {
+
+    private Timer timer;
+    private static final int THRESHOLD = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +24,25 @@ public class Puzzle29Activity extends AppCompatActivity {
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN: {
+                timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        anim(0);
+                    }
+                }, THRESHOLD);
                 break;
             }
             case MotionEvent.ACTION_UP: {
+                timer.cancel();
                 break;
             }
             default:
         }
         return true;
+    }
+
+    void anim(int index) {
+        animation(this, index);
     }
 }
