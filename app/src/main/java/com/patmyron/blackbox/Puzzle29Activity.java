6b97611362f41e4b693d0dd3863a43f92892443a
Pat Myron <pmyron09@gmail.com>
2017-11-11 16:09:25 -0800

    starting touch puzzle

--- app/src/main/java/com/patmyron/blackbox/Puzzle29Activity.java
+++ app/src/main/java/com/patmyron/blackbox/Puzzle29Activity.java
@@ -2,6 +2,7 @@ package com.patmyron.blackbox;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 
 public class Puzzle29Activity extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class Puzzle29Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle29);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN: {
+                break;
+            }
+            case MotionEvent.ACTION_UP: {
+                break;
+            }
+            default:
+        }
+        return true;
     }
 }
