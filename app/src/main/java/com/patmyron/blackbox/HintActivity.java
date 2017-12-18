231c5f45d87ed391a6aafa465fa77f00b9612ddb
Pat Myron <pmyron09@gmail.com>
2017-12-18 00:04:29 -0800

    get that intent

--- app/src/main/java/com/patmyron/blackbox/HintActivity.java
+++ app/src/main/java/com/patmyron/blackbox/HintActivity.java
@@ -1,7 +1,8 @@
 package com.patmyron.blackbox;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class HintActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class HintActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hint);
+        Intent intent = getIntent();
     }
 }
